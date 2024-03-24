d1d71fe61b1e9f7cb0361e613340640eb4b165c5
Alexandre Ferreira <shieldforce2@gmail.com>
2024-03-24 18:45:03 -0300

    finalizado feature

--- app/Services/CallbackApiLoginService.php
+++ app/Services/CallbackApiLoginService.php
@@ -4,6 +4,8 @@ namespace App\Services;
 
 use App\Http\Requests\ApiLogin\CallbackRequest;
 use App\Models\User;
+use Exception;
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Http;
@@ -12,35 +14,11 @@ class CallbackApiLoginService
 {
     public function run(CallbackRequest $request)
     {
-        $data        = $request->validated();
-        $baseApiAuth = env("API_AUTH_URL");
-
-        $response = Http::asForm()->post("{$baseApiAuth}/oauth/token", [
-            'grant_type'    => "authorization_code",
-            'client_id'     => env("CLIENT_ID"),
-            'client_secret' => env("CLIENT_SECRET"),
-            'redirect_uri'  => env("CLIENT_REDIRECT"),
-            'code'          => $data["code"],
-        ]);
-
-        $token_type   = $response->json()['token_type'];
-        $access_token = $response->json()['access_token'];
-
-        $responseUser = Http::withHeaders([
-            'Accept'        => 'application/json',
-            'Authorization' => "{$token_type} {$access_token}",
-        ])->get("{$baseApiAuth}/api/user");
-
-        $encryptUser = Crypt::encrypt($responseUser);
-
-        $user = User::updateOrCreate([
-            'name'  => $responseUser["name"],
-            'email' => $responseUser["email"],
-        ], [
-            'password' => bcrypt($encryptUser),
-        ]);
-
-        Auth::login($user);
+        $data         = $request->validated();
+        $baseApiAuth  = env("API_AUTH_URL");
+        $response     = $this->authorizationResponse($baseApiAuth, $data);
+        $responseUser = $this->userResponse($baseApiAuth, $response);
+        $this->authUser($this->userSave($responseUser));
 
         if (Auth::check()) {
             #TODO: implements scopes (ACL)
@@ -48,5 +26,42 @@ class CallbackApiLoginService
             return redirect()->route("dashboard");
         }
 
+        throw new Exception("Erro ao redirecionar para Dashboard!");
+    }
+
+    private function authorizationResponse($baseApiAuth, $data): Response
+    {
+        return Http::asForm()->post("{$baseApiAuth}/oauth/token", [
+            'grant_type'    => "authorization_code",
+            'client_id'     => env("CLIENT_ID"),
+            'client_secret' => env("CLIENT_SECRET"),
+            'redirect_uri'  => env("CLIENT_REDIRECT"),
+            'code'          => $data["code"],
+        ]);
+    }
+
+    private function userResponse($baseApiAuth, $response): Response
+    {
+        $token_type   = $response->json()['token_type'];
+        $access_token = $response->json()['access_token'];
+        return Http::withHeaders([
+            'Accept'        => 'application/json',
+            'Authorization' => "{$token_type} {$access_token}",
+        ])->get("{$baseApiAuth}/api/user");
+    }
+
+    private function userSave($responseUser): User
+    {
+        return User::updateOrCreate([
+            'name'  => $responseUser["name"],
+            'email' => $responseUser["email"],
+        ], [
+            'password' => bcrypt(Crypt::encrypt($responseUser)),
+        ]);
+    }
+
+    private function authUser(User $user): void
+    {
+        Auth::login($user);
     }
 }
